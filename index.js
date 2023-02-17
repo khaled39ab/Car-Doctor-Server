9795561b2b01e70d39e93dc8e43a6fc0e332565f
khaled39ab <khaled39ab@gmail.com>
2023-02-17 23:45:57 +0600

    insert document menually on db

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 4000;
+
 const app = express();
 
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.MOTOR_USER}:${process.env.MOTOR_PASSWORD}@cluster0.mxmpcyg.mongodb.net/?retryWrites=true&w=majority`;
-
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-
-
+//----  Middleware ---------
 app.use(cors());
 app.use(express.json());
 
 
-app.get('/', (req, res) =>{
+//++++++++++   mongodb connection  ++++++++++++++++++
+const uri = `mongodb+srv://${process.env.MOTOR_USER}:${process.env.MOTOR_PASSWORD}@cluster0.mxmpcyg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+const run = async () => {
+
+    try {
+        const servicesCollection = client.db("MotorService").collection("services");
+
+    }
+    finally {
+
+    }
+}
+
+
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
     res.send("Server is running")
 });
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log('Server running from', port);
 })
