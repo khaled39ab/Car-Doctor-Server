fd520affc7603d5c113b0e16837996c908c0bc98
khaled39ab <khaled39ab@gmail.com>
2023-02-17 19:41:29 +0600

    server connected

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 4000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) =>{
+    res.send("Server is running")
+});
+
+app.listen(port, () =>{
+    console.log('Server running from', port);
+})
