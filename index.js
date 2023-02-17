2930df810069a887eb943114e565626abdcce59a
khaled39ab <khaled39ab@gmail.com>
2023-02-17 23:29:53 +0600

    connected with mongodb

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const port = process.env.PORT || 4000;
 const app = express();
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.MOTOR_USER}:${process.env.MOTOR_PASSWORD}@cluster0.mxmpcyg.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.use(cors());
 app.use(express.json());
